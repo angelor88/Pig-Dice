0f5ce60cddb30d24c237e2ccc258f9ffb3156772
Ainur Dzhaianbaeva and Angelo Russoniello <angelo@>
2018-01-25 11:09:09 -0800

    adds and interfce logic for the entry page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 //the user interface logic
+$(document).ready(function() {
+  $("button#startGame").submit(function(event){
+    event.preventDefault()
 
+    player1 = new Player(true);
+    player2 = new Player(false);
+    $(".entry-page").show();
+    $(".game-page").hide();
+
+
+    var playerOne = $("input#player1").val();
+    $("#playerOneName").text(playerOne);
+    var playerTwo = $("input#player2").val();
+    $("#playerTwoName").text(playerTwo);
+
+
+
+  });
+});
 
 //business logic
