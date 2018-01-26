ab264204f724177678115a6815ff6b9932a4a718
Ainur Dzhaianbaeva and Angelo Russoniello <ainur@>
2018-01-25 16:01:16 -0800

    adds interface logic to show the roll results for players1 and player 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,50 @@
+//business logic
+var player1="";
+var player2="";
+
+// we need to add a fuction to throw the dice
+
+function Player(turn) {
+  // this.turn = turn;
+  this.playerName;
+}
+
+
 //the user interface logic
 $(document).ready(function() {
-  $("button#startGame").submit(function(event){
-    event.preventDefault()
-
+  $("button#start").click(function(event){
+    //Constructor
     player1 = new Player(true);
     player2 = new Player(false);
-    $(".entry-page").show();
-    $(".game-page").hide();
+    $(".player-console").show();
+    $(".start-menu").hide();
+    //Variable that hold name number 1
+    var player1Name = $(".player1Name").val();
+    $("#player1Name").text(player1Name);
+    //Variable that hold name number 2
+    var player2Name = $(".player2Name").val();
+    $("#player2Name").text(player2Name);
 
-
-    var playerOne = $("input#player1").val();
-    $("#playerOneName").text(playerOne);
-    var playerTwo = $("input#player2").val();
-    $("#playerTwoName").text(playerTwo);
-
-
+    player1.playerName = player1Name;
+    player2.playerName = player2Name;
 
   });
-});
+//shows the result for the Player 1.
+  $("button#player1-roll").click(function(event){
+    player1.roll = throwdice();
+    $("#die-roll-1").text(player1.roll);
+    player1.rollone();
+    $("totalScore#round-total-1").text(player1.tempscore);
+  });
 
-//business logic
+
+  // Shows the result for the Prayer 2.
+  $("button#player2-roll").click(function(event){
+    player2.roll = throwdice();
+    $("#die-roll-2").text(player2.roll);
+    player2.rollone();
+    $("totalScore#round-total-2").text(player2.tempscore);
+  });
+
+
+});
